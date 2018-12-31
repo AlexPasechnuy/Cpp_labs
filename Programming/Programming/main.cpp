1ec0c4c9102aadda25f2afdd21032f69628f1453
AlexPasechnuy <alexpasechnuy@gmail.com>
2018-12-31 14:28:31 +0200

    Interface is changed

--- Programming/Programming/main.cpp
+++ Programming/Programming/main.cpp
@@ -14,23 +14,23 @@
 #include "lab5.h"
 #include "lab6.h"
 
-int main()
+void firstSem();
+void secondSem();
+void secProg();
+void secAlg();
+
+void firstSem()
 {
 	while (true)
 	{
 		system("cls");
 		std::cout << "Select your lab:" << std::endl;
-		std::vector<std::string> menu = { "Lab 1", "Lab 2", "Lab 3",
-			"Lab 4", "Lab 5", "Lab 6"};
-		switch (Menu::getInstance().menuOrgan(menu))
+		std::vector<std::string> labs = { "Lab 1", "Lab 2", "Lab 3",
+			"Lab 4", "Lab 5", "Lab 6" };
+		switch (Menu::getInstance().menuOrgan(labs))
 		{
 		case -1:
-			system("cls");
-			std::cout << "Thanks for using my product)\n"
-				<< "With love, Alex Pasechnuy\n";
-			system("pause");
-			return 0;
-			break;
+			return;
 		case 0:
 		{
 			Lab1 find;
@@ -45,22 +45,129 @@ int main()
 		{
 			Lab3 find;
 			find.useLab3();
-		}
+		}break;
 		case 3:
 		{
 			UseLab4 find;
 			find.useLab3();
-		}
+		}break;
 		case 4:
 		{
 			UseLab5 find;
 			find.useLab5();
-		}
+		}break;
 		case 5:
 		{
 			UseLab6 find;
 			find.useLab6();
 		}
+		}
+	}
+}
+
+void secondSem()
+{
+	while (true)
+	{
+		system("cls");
+		std::cout << "Select your subject:" << std::endl;
+		std::vector<std::string> subj = { "Algorithmization", "Programming" };
+		switch (Menu::getInstance().menuOrgan(subj))
+		{
+		case -1:
+			return;
+			break;
+		case 0:
+		{
+			secAlg();
+		}break;
+		case 1:
+		{
+			secProg();
+		}break;
+		}
+	}
+}
+
+void secProg()
+{
+	while (true)
+	{
+		system("cls");
+		std::cout << "Select your lab:" << std::endl;
+		std::vector<std::string> labs = { "Lab 1", "Lab 2", "Lab 3" };
+		switch (Menu::getInstance().menuOrgan(labs))
+		{
+		case -1:
+			return;
+			break;
+		case 0:
+		{
+			
+		}break;
+		case 1:
+		{
+			
+		}break;
+		case 2:
+		{
+
+		}break;
+		}
+	}
+}
+
+void secAlg()
+{
+	while (true)
+	{
+		system("cls");
+		std::cout << "Select your lab:" << std::endl;
+		std::vector<std::string> labs = { "Lab 1", "Lab 2", "Lab 3" };
+		switch (Menu::getInstance().menuOrgan(labs))
+		{
+		case -1:
+			return;
+			break;
+		case 0:
+		{
+
+		}break;
+		case 1:
+		{
+
+		}break;
+		case 2:
+		{
+
+		}break;
+		}
+	}
+}
+
+int main()
+{
+	bool isExit = false;
+	while (isExit == false)
+	{
+		system("cls");
+		std::cout << "Select your semester:" << std::endl;
+		std::vector<std::string> semesters = { "First", "Second"};
+		switch (Menu::getInstance().menuOrgan(semesters))
+		{
+		case -1:		//exit
+			system("cls");
+			std::cout << "Thanks for using my product)\n"
+					<< "With love, Alex Pasechnuy\n";
+			system("pause");
+			return 0;
+			break;
+		case 0:	
+			firstSem();
+		break;
+		case 1:
+			secondSem();
+		break;
 		}
 	}
 }
